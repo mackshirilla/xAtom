77fef7afd1c159839349c71564bd0407599ae4e9
mackshirilla <mack@bigstachedesign.com>
2024-04-23 20:08:59 -0400

    Commit

--- build/assets/js/79702f44.a248a92f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkxatom_document=self.webpackChunkxatom_document||[]).push([[812],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
--- build/assets/js/894.7650562c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkxatom_document=self.webpackChunkxatom_document||[]).push([[894],{8894:(e,t,u)=>{u.r(t)}}]);
